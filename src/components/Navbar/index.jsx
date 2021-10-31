bfc38b7766dba1029160ec8793bc1d07fb09c4a2
TommyTraddles <jandresnova@gmail.com>
2021-10-31 23:24:49 +0100

    wip: adding Dark mode base

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,22 +1,37 @@
-import { HStack } from '@chakra-ui/react'
+import { HStack, IconButton } from '@chakra-ui/react'
 import { Card } from 'components/Card'
 import { NavLink } from 'react-router-dom'
+import { paths } from 'services/routes'
 
 export const Navbar = () => {
   return (
     <>
       <Card
-        // pos="absolute" 
-        // bottom="0" 
-        // left="0"
-        // rigth='0'
+      // pos="absolute"
+      // bottom="0"
+      // left="0"
+      // rigth='0'
       >
-        <HStack justifyContent='space-between'>
-          <NavLink to="/"> home </NavLink>
-          <NavLink to="/new"> new </NavLink>
-          <NavLink to="/history"> history </NavLink>
-          <NavLink to="/leaderboard"> leaderboard </NavLink>
-          <NavLink to="/me"> me </NavLink>
+        <HStack>
+          <IconButton>
+            <NavLink to={paths.home}> home </NavLink>
+          </IconButton>
+
+          <IconButton>
+            <NavLink to={paths.calculator}> new </NavLink>
+          </IconButton>
+
+          <IconButton>
+            <NavLink to={paths.history}> history </NavLink>
+          </IconButton>
+
+          <IconButton>
+            <NavLink to={paths.leaderboard}> leaderboard </NavLink>
+          </IconButton>
+          
+          <IconButton>
+            <NavLink to={paths.profile}> me </NavLink>
+          </IconButton>
         </HStack>
       </Card>
     </>
