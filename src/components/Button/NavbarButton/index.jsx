960f2e619e1a126db11ba540bac2d1eb5b436319
TommyTraddles <jandresnova@gmail.com>
2021-11-01 10:26:57 +0100

    wip: adding color when selected

--- src/components/Button/NavbarButton/index.jsx
+++ src/components/Button/NavbarButton/index.jsx
@@ -2,10 +2,12 @@ import { VStack, Button, Text } from '@chakra-ui/react'
 import { NavLink } from 'react-router-dom'
 
 export const NavbarButton = ({ path, icon, name }) => {
+  const pathname = window.location.pathname
+
   return (
     <>
       <NavLink to={path}>
-        <Button variant='ghost' w={12} h={12}>
+        <Button colorScheme={  pathname === path ? 'primary' : 'gray' } variant='ghost' w={12} h={12}>
           <VStack>
             {icon}
             <Text fontSize="xs"> {name} </Text>
