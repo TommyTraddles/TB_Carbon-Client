b2d5e7f80595783bbe2956f709bc1f25ba61db6f
TommyTraddles <jandresnova@gmail.com>
2021-11-05 18:49:43 +0100

    wip: update profile

--- src/components/User/Preferences/Update/index.jsx
+++ src/components/User/Preferences/Update/index.jsx
@@ -1,13 +1,41 @@
-import { Heading, Text } from '@chakra-ui/react'
+// component
+import { Heading, Input } from '@chakra-ui/react'
+// hooks
+// import { useState } from 'react'
 
 export const UpdateUser = () => {
+  // inputs
+  // const form = { email: '', password: '' }
+  // const [info, setinfo] = useState(form)
+  // const handleInput = (e) =>
+  //   setinfo((curr) => ({ ...curr, [e.target.name]: e.target.value }))
+  // const handleSubmit = (e) => {
+  //   e.preventDefault()
+  //   console.info(info)
+  // }
+  // state
+  // const isValid = info.email !== '' && info.password !== ''
+  // const error = false
+
   return (
     <>
-      <Heading size="xs"> Update </Heading>
-      <Text> Image </Text>
-      <Text> Email </Text>
-      <Text> User </Text>
-      <Text> Pass </Text>
+      <Heading size="xs"> Account settings </Heading>
+
+      <form method="POST">
+        <Heading size="xs"> Profile pic </Heading>
+        <Input type="file" />
+      </form>
+
+      <form method="POST">
+        <Heading size="xs"> Change Username </Heading>
+        <Input type="text" />
+      </form>
+
+      <form method="POST">
+        <Heading size="xs"> Change password </Heading>
+        <Input type="password" />
+        {/* <InputPass handleInput={handleInput} info={info} error={error} /> */}
+      </form>
     </>
   )
 }
