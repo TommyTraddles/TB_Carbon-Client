33dedb10c1f754e6a7ff8d6df9c4c54ec15ab1fc
TommyTraddles <jandresnova@gmail.com>
2021-11-09 15:52:31 +0100

    wip: adding register Routing on forgot-pass

--- src/pages/Auth/Forgot-pass/index.jsx
+++ src/pages/Auth/Forgot-pass/index.jsx
@@ -1,10 +1,20 @@
 // components
-import { ForgotForm } from "components/Auth/Forgot-pass/Form"
+import { Text, HStack, Button } from '@chakra-ui/react'
+import { ForgotForm } from 'components/Auth/Forgot-pass/Form'
+// data
+import { paths } from 'services/routes'
+import { Link } from 'react-router-dom'
 
 export const Forgot = () => {
   return (
     <>
       <ForgotForm />
+      <HStack>
+        <Text> ¿No tienes cuenta? </Text>
+        <Button variant="link">
+          <Link to={paths.register}> Registro </Link>
+        </Button>
+      </HStack>
     </>
   )
 }
