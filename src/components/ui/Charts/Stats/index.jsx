b2ac5b0e9515e7e901dbf0079e28492823067006
TommyTraddles <jandresnova@gmail.com>
2021-11-16 22:59:19 +0100

    style: direction

--- src/components/ui/Charts/Stats/index.jsx
+++ src/components/ui/Charts/Stats/index.jsx
@@ -4,33 +4,32 @@ import {
   StatNumber,
   StatHelpText,
   StatArrow,
-  StatGroup,
   HStack,
 } from '@chakra-ui/react'
 
 export const StatCard = () => {
   return (
     <>
-      <HStack>
-        <StatGroup>
-          <Stat>
-            <StatLabel>Sent</StatLabel>
-            <StatNumber>345,670</StatNumber>
-            <StatHelpText>
-              <StatArrow type="increase" />
-              23.36%
-            </StatHelpText>
-          </Stat>
-          
-          <Stat>
+      <HStack justifyContent="space-between" w="full">
+        <Stat>
+          <StatLabel>Sent</StatLabel>
+          <StatNumber>345,670</StatNumber>
+          <StatHelpText>
+            <StatArrow type="increase" />
+            23.36%
+          </StatHelpText>
+        </Stat>
+
+        <Stat>
+          <HStack>
             <StatLabel>Clicked</StatLabel>
-            <StatNumber>45</StatNumber>
-            <StatHelpText>
-              <StatArrow type="decrease" />
-              9.05%
-            </StatHelpText>
-          </Stat>
-        </StatGroup>
+          </HStack>
+          <StatNumber>45</StatNumber>
+          <StatHelpText>
+            <StatArrow type="decrease" />
+            9.05%
+          </StatHelpText>
+        </Stat>
       </HStack>
     </>
   )
