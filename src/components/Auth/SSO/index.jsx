e35d67f8c31705864ccbde5e1a223266d5a98729
TommyTraddles <jandresnova@gmail.com>
2021-11-16 22:58:30 +0100

    chore: deleting SSO options

--- src/components/Auth/SSO/index.jsx
+++ src/components/Auth/SSO/index.jsx
@@ -7,21 +7,11 @@ export const SSO = () => {
   return (
     <>
       <HStack my={2}>
-          <Button colorScheme="facebook" w="full" variant="outline">
-        <Link href={SSOpaths.facebook} isExternal>
-            Facebook
-        </Link>
-          </Button>
-          <Button colorScheme="gray" w="full" variant="outline">
-        <Link href={SSOpaths.google} isExternal>
+        <Button colorScheme="gray" w="full" variant="outline">
+          <Link href={SSOpaths.google} isExternal>
             Google
-        </Link>
-          </Button>
-          <Button colorScheme="gray" w="full" variant="outline">
-        <Link href={SSOpaths.apple} isExternal>
-            Apple
-        </Link>
-          </Button>
+          </Link>
+        </Button>
       </HStack>
     </>
   )
